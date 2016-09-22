98d906232b2c1eec3fe800e33172d3feed409795
Doug Swain <dswain@gmail.com>
2016-09-22 08:40:49 -0400

    Implemented the functionality for mean, median, and mode. This includes a small set of tests for each one to check the expected functionality. Note that lodash's interface has not been added, so the types have not be formally checked yet.

--- src/stats.js
+++ src/stats.js
@@ -1,4 +1,48 @@
 // @flow
 
+import { countBy, invertBy, isNaN, isSafeInteger, map, reduce, sortBy, sum, toSafeInteger } from 'lodash';
+
+const toNumber = (value: string): number => {
+    const parsedValue = parseFloat(value);
+
+    if (!isNaN(parsedValue) && isSafeInteger(parsedValue)) {
+        return toSafeInteger(parsedValue);
+    } else if (!isNaN(parsedValue) && !isSafeInteger(parsedValue)) {
+        return parsedValue;
+    }
+
+    throw new Error('the value given (which was ' + value + ') is not a number at all!');
+}
+
 export const mean = (values: number[]): number =>
-    values.length > 0 ? values.reduce((sum, next) => sum + next) / values.length : 0;
+    sum(values) / values.length;
+
+export const median = (values: number[]): number => {
+    const sorted = sortBy(values);
+
+    if (sorted.length > 0 && sorted.length % 2 !== 0) {
+        return sorted[Math.floor(sorted.length / 2)];
+    } else if (sorted.length > 0 && sorted.length % 2 === 0) {
+        const midIndex = Math.floor(sorted.length / 2);
+        const lower = sorted[midIndex - 1];
+        const upper = sorted[midIndex];
+
+        return (upper + lower) / 2;
+    }
+
+    return NaN;
+}
+
+export const mode = (values: number[]): number[] => {
+    const tallyTable = invertBy(countBy(values));
+    const maxValue = reduce(tallyTable, (result, value, key) => Math.max(toNumber(result), toNumber(key)));
+
+    const result = map(tallyTable[maxValue], toNumber);
+    return result;
+}
+
+export const standardDev = (values: number[]): number =>
+    0
+
+export const variance = (values: number[]): number =>
+    0
--- test/stats.spec.js
+++ test/stats.spec.js
@@ -2,13 +2,13 @@
 
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { mean } from '../src/stats.js';
+import { mean, median, mode, standardDev, variance } from '../src/stats.js';
 
 describe('Stats module', () => {
     describe('the mean of a set of numbers', () => {
-        it('is zero when no numbers are specificed', () => {
+        it('is not a number when no numbers are specificed', () => {
             // expect(mean(undefined)).to.equal(0); # This case is invalid when running flow
-            expect(mean([])).to.equal(0);
+            expect(mean([])).to.be.NaN;
         });
 
         it('is equal to itself when only a single number is provided', () => {
@@ -24,5 +24,55 @@ describe('Stats module', () => {
         it('is equal to 4.5 when the set of numbers is [3,4,5,6]', () => {
             expect(mean([3, 4, 5, 6])).to.equal(4.5);
         });
+    });
+
+    describe('the median of a set of numbers', () => {
+        it('is NaN when no numbers are specified', () => {
+            expect(median([])).to.be.NaN;
+        });
+
+        it('is the first number when a list of size 1 is given', () => {
+            expect(median([15])).to.equal(15);
+        });
+
+        it('is the second number when a list of size 3 is given', () => {
+            expect(median([3, 6, 1024])).to.equal(6);
+        });
+
+        it('is between the 1st and 2nd number when a list of size 2 is given', () => {
+            expect(median([2, 4])).to.equal(3);
+        });
+
+        it('is still the midpoint of numbers regardless of order', () => {
+            expect(median([1024, 12, 144444, 13, 1924, 169, 4])).to.equal(169);
+        });
+    });
+
+    describe('the mode of a set of numbers', () => {
+        it('is an empty set when no values are given', () => {
+            expect(mode([])).to.eql([]);
+        });
+
+        it('contains only 5 when it is the most frequent numbers', () => {
+            const result = mode([5, 1, 4, 2, 5, 2, 5]);
+
+            expect(result).to.have.lengthOf(1);
+            expect(result).to.have.all.members([5]);
+        });
+
+        it('contains both 5, 10, and 1 when they are the most frequent numbers', () => {
+            const result = mode([1, 5, 10, 5, 2, 3, 10, 1, 4, 15])
+
+            expect(result).to.have.lengthOf(3);
+            expect(result).to.have.all.members([5, 10, 1]);
+        });
+    });
+
+    describe('the standard deviation of a set of numbers', () => {
+
+    });
+
+    describe('the variance of a set of numbers', () => {
+
     });
 });
